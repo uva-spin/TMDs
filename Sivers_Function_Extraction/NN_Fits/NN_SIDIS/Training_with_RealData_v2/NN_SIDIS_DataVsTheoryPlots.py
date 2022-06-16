27410751b409758f6cdca616bc32392149bd0309
isharafernando <isharafernando@gmail.com>
2022-06-16 19:46:12 -0400

    Added files for RealData NN fit test v2

---
+++ Sivers_Function_Extraction/NN_Fits/NN_SIDIS/Training_with_RealData_v2/NN_SIDIS_DataVsTheoryPlots.py
@@ -0,0 +1,279 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import lhapdf
+import matplotlib.pyplot as plt
+#import functions_develop
+import natsort
+import os
+
+Models_folder = '/scratch/cee9hc/Sivers_NN_Models/Train_Real/Models_SIDIS_v4'
+folders_array=os.listdir(Models_folder)
+folders_array=natsort.natsorted(folders_array)
+numreplicas_SIDIS=len(folders_array)
+print(numreplicas_SIDIS)
+
+
+herm9_DATA = pd.read_csv('./Data/HERMES_p_2009.csv').dropna(axis=0, how='all').dropna(axis=1, how='all')
+herm20_DATA = pd.read_csv('./Data/HERMES_p_2020.csv').dropna(axis=0, how='all').dropna(axis=1, how='all')
+comp9_DATA = pd.read_csv('./Data/COMPASS_d_2009.csv').dropna(axis=0, how='all').dropna(axis=1, how='all')
+comp15_DATA = pd.read_csv('./Data/COMPASS_p_2015.csv').dropna(axis=0, how='all').dropna(axis=1, how='all')
+
+#herm9 = pd.read_csv('./Pseudo_Data_Set/Pseudo_SIDIS_HERMES2009.csv').dropna(axis=0, how='all').dropna(axis=1, how='all')
+#herm20 = pd.read_csv('./Pseudo_Data_Set/Pseudo_SIDIS_HERMES2020.csv').dropna(axis=0, how='all').dropna(axis=1, how='all')
+#comp9 = pd.read_csv('./Pseudo_Data_Set/Pseudo_SIDIS_COMPASS2009.csv').dropna(axis=0, how='all').dropna(axis=1, how='all')
+#comp15 = pd.read_csv('./Pseudo_Data_Set/Pseudo_SIDIS_COMPASS2015.csv').dropna(axis=0, how='all').dropna(axis=1, how='all')
+
+######################################################################################
+############### HERE the User need to define: plots for which data set (kinematics)###
+######################################################################################
+
+#dfsingle = pd.concat([herm20])
+#DataSet = 'HERMES2020'
+
+#HERMES2009 = pd.concat([herm9])
+#HERMES2020 = pd.concat([herm20])
+#COMPASS2009 = pd.concat([comp9])
+#COMPASS2015 = pd.concat([comp15])
+
+HERMES2009 = pd.concat([herm9_DATA])
+HERMES2020 = pd.concat([herm20_DATA])
+COMPASS2009 = pd.concat([comp9_DATA])
+COMPASS2015 = pd.concat([comp15_DATA])
+
+EPOCHS = '20000'
+LR = '0001'
+HL = '2'
+NODES = '256'
+LOSSFN = 'Chi2'
+
+def calc_yhat(model, X):
+    return model.predict(X)
+
+
+class A0(tf.keras.layers.Layer):
+    def __init__(self, kperp2avg=.57, pperp2avg=.12, **kwargs):
+        super(A0, self).__init__(name='a0')
+        self.m1 = tf.Variable(1., name='m1')
+        self.kperp2avg = kperp2avg
+        self.pperp2avg = pperp2avg
+        self.e = tf.constant(1.)
+
+    def get_config(self):
+        config = super().get_config().copy()
+
+        config.update({
+            'kperp2avg': self.kperp2avg,
+            'pperp2avg': self.pperp2avg
+        })
+
+        return config
+
+    def call(self, inputs):
+        z = inputs[:, 0]
+        pht = inputs[:, 1]
+        ks2avg = (self.kperp2avg*self.m1**2)/(self.m1**2 + self.kperp2avg) #correct
+        topfirst = (z**2 * self.kperp2avg + self.pperp2avg) * ks2avg**2 #correct
+        bottomfirst = (z**2 * ks2avg + self.pperp2avg)**2 * self.kperp2avg #correct
+        exptop = pht**2 * z**2 * (ks2avg - self.kperp2avg) #correct
+        expbottom = (z**2 * ks2avg + self.pperp2avg) * (z**2 * self.kperp2avg + self.pperp2avg) #correct
+        last = tf.sqrt(2*self.e) * z * pht / self.m1 #correct
+        return (topfirst/bottomfirst) * tf.exp(-exptop/expbottom) * last
+
+
+class Quotient(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(Quotient, self).__init__()
+
+    def call(self, inputs):
+        if len(inputs) != 2 or inputs[0].shape[1] != 1:
+            raise Exception('must be two tensors of shape (?, 1)')
+        return inputs[0]/inputs[1]
+
+
+
+class DataANN(object):
+    def __init__(self, pdfset='cteq61',
+                 ff_PIp='NNFF10_PIp_nlo', ff_PIm='NNFF10_PIm_nlo', ff_PIsum='NNFF10_PIsum_nlo',
+                 ff_KAp='NNFF10_KAp_nlo', ff_KAm='NNFF10_KAm_nlo'):
+        '''
+        Get data in proper format for neural network
+        '''
+        self.pdfData = lhapdf.mkPDF(pdfset)
+        self.ffDataPIp = lhapdf.mkPDF(ff_PIp, 0)
+        self.ffDataPIm = lhapdf.mkPDF(ff_PIm, 0)
+        self.ffDataPIsum = lhapdf.mkPDF(ff_PIsum, 0)
+        self.ffDataKAp = lhapdf.mkPDF(ff_KAp, 0)
+        self.ffDataKAm = lhapdf.mkPDF(ff_KAm, 0)
+        # needs to be extended to generalize for kaons
+        self.eu = 2/3
+        self.eubar = -2/3
+        self.ed = -1/3
+        self.edbar = 1/3
+        self.es = -1/3
+        self.esbar = 1/3
+
+        self.ffDict = {0: self.ffDataPIp,
+                       1: self.ffDataPIm,
+                       2: self.ffDataPIsum,
+                       3: self.ffDataKAp,
+                       4: self.ffDataKAm}
+
+
+    def pdf(self, flavor, x, QQ):
+        return np.array([self.pdfData.xfxQ2(flavor, ax, qq) for ax, qq in zip(x, QQ)])
+
+
+    def ff(self, func, flavor, z, QQ):
+        return np.array([func.xfxQ2(flavor, az, qq) for az, qq in zip(z, QQ)])
+
+
+    def makeData(self, df, hadrons, dependencies):
+
+        data = {'x': [],
+             'z': [],
+             'phT': [],
+             'uexpr': [],
+             'ubarexpr': [],
+             'dexpr': [],
+             'dbarexpr': [],
+             'sexpr': [],
+             'sbarexpr': []}
+
+        y = []
+        err = []
+
+        df = df.loc[df['hadron'].isin(hadrons), :]
+        df = df.loc[df['1D_dependence'].isin(dependencies), :]
+        #X = np.array(df[['x', 'z', 'phT', 'Q2', 'hadron']])
+        for i, had in enumerate(['pi+', 'pi-', 'pi0', 'k+', 'k-']):
+            sliced = df.loc[df['hadron'] == had, :]
+            y += list(sliced['Siv'])
+            err += list(sliced['tot_err'])
+
+            x = sliced['x']
+            z = sliced['z']
+            QQ = sliced['Q2']
+            data['uexpr'] += list(self.eu**2 * self.pdf(2, x, QQ) * self.ff(self.ffDict[i], 2, z, QQ))
+            data['ubarexpr'] += list(self.eubar**2 * self.pdf(-2, x, QQ) * self.ff(self.ffDict[i], -2, z, QQ))
+            data['dexpr'] += list(self.ed**2 * self.pdf(1, x, QQ) * self.ff(self.ffDict[i], 1, z, QQ))
+            data['dbarexpr'] += list(self.edbar**2 * self.pdf(-1, x, QQ) * self.ff(self.ffDict[i], -1, z, QQ))
+            data['sexpr'] += list(self.es**2 * self.pdf(3, x, QQ) * self.ff(self.ffDict[i], 3, z, QQ))
+            data['sbarexpr'] += list(self.esbar**2 * self.pdf(-3, x, QQ) * self.ff(self.ffDict[i], -3, z, QQ))
+
+            data['x'] += list(x)
+            data['z'] += list(z)
+            data['phT'] += list(sliced['phT'])
+
+        for key in data.keys():
+            data[key] = np.array(data[key])
+
+        return data, data[dependencies[0]], np.array(y), np.array(err)
+
+
+datann = DataANN()
+
+def SiversAsym(df,numReplicas,hadron,dependence):
+    T_Xplt, T_DEP, T_yplt, T_errplt = datann.makeData(df, [hadron], [dependence])
+    results = []
+    for i in range(numReplicas):
+        testmodel = tf.keras.models.load_model(str(Models_folder)+'/rep' + str(i) + '.h5',custom_objects={'A0': A0, 'Quotient': Quotient})
+        results.append(list(calc_yhat(testmodel, T_Xplt)))
+    return np.array(results)
+
+
+def plotSivAsymmBands(datasetdf, numReplicas,hadron,dependence, numSigma=1):
+    T_Xplt, T_DEP, T_yplt, T_errplt = datann.makeData(datasetdf, [hadron], [dependence])
+    results = SiversAsym(datasetdf, numReplicas,hadron,dependence)
+    Yhat = np.array(results.mean(axis=0))
+    Yhat = Yhat.flatten()
+    Yerr = np.array(results.std(axis=0))
+    Yerr = Yerr.flatten()
+    #plt.fill_between(T_DEP, Yhat-numSigma*Yerr, Yhat+numSigma*Yerr, color='r', alpha=0.3)
+    plt.errorbar(T_DEP, T_yplt,yerr=T_errplt, fmt='bo',label='Data')
+    plt.errorbar(T_DEP, Yhat, yerr=numSigma*Yerr, fmt='ro',label='NN')
+    #plt.plot(T_DEP, Yhat, 'r',label='ANN')
+    #### Here the user needs to define the plot title based on the data set ####
+    plt.title('SIDIS Sivers '+str(hadron),fontsize=15)
+    #plt.ylim([-0.001,0.001])
+    plt.xlabel(str(dependence),fontsize=15)
+    plt.legend(loc=2,fontsize=15,handlelength=3)
+
+
+
+fig1=plt.figure(1,figsize=(15,10))
+plt.subplot(2,3,1)
+plotSivAsymmBands(HERMES2009,numreplicas_SIDIS,'pi+','x')
+plt.subplot(2,3,2)
+plotSivAsymmBands(HERMES2009,numreplicas_SIDIS,'pi+','z')
+plt.subplot(2,3,3)
+plotSivAsymmBands(HERMES2009,numreplicas_SIDIS,'pi+','phT')
+plt.subplot(2,3,4)
+plotSivAsymmBands(HERMES2009,numreplicas_SIDIS,'k+','x')
+plt.subplot(2,3,5)
+plotSivAsymmBands(HERMES2009,numreplicas_SIDIS,'k+','z')
+plt.subplot(2,3,6)
+plotSivAsymmBands(HERMES2009,numreplicas_SIDIS,'k+','phT')
+#### Here the user needs to define the plot title based on the data set ####
+plt.savefig('HERMES09_'+str(LOSSFN)+'_'+str(EPOCHS)+'Ep_'+str(HL)+'H_'+str(NODES)+'N_'+str(LR)+'LR.pdf', format='pdf', bbox_inches='tight')
+
+fig2=plt.figure(2,figsize=(15,10))
+plt.subplot(2,3,1)
+plotSivAsymmBands(HERMES2020,numreplicas_SIDIS,'pi+','x')
+plt.subplot(2,3,2)
+plotSivAsymmBands(HERMES2020,numreplicas_SIDIS,'pi+','z')
+plt.subplot(2,3,3)
+plotSivAsymmBands(HERMES2020,numreplicas_SIDIS,'pi+','phT')
+plt.subplot(2,3,4)
+plotSivAsymmBands(HERMES2020,numreplicas_SIDIS,'k+','x')
+plt.subplot(2,3,5)
+plotSivAsymmBands(HERMES2020,numreplicas_SIDIS,'k+','z')
+plt.subplot(2,3,6)
+plotSivAsymmBands(HERMES2020,numreplicas_SIDIS,'k+','phT')
+#### Here the user needs to define the plot title based on the data set ####
+plt.savefig('HERMES20_'+str(LOSSFN)+'_'+str(EPOCHS)+'Ep_'+str(HL)+'H_'+str(NODES)+'N_'+str(LR)+'LR.pdf', format='pdf', bbox_inches='tight')
+
+
+fig3=plt.figure(3,figsize=(15,10))
+plt.subplot(2,3,1)
+plotSivAsymmBands(COMPASS2009,numreplicas_SIDIS,'pi+','x')
+plt.subplot(2,3,2)
+plotSivAsymmBands(COMPASS2009,numreplicas_SIDIS,'pi+','z')
+plt.subplot(2,3,3)
+plotSivAsymmBands(COMPASS2009,numreplicas_SIDIS,'pi+','phT')
+plt.subplot(2,3,4)
+plotSivAsymmBands(COMPASS2009,numreplicas_SIDIS,'k+','x')
+plt.subplot(2,3,5)
+plotSivAsymmBands(COMPASS2009,numreplicas_SIDIS,'k+','z')
+plt.subplot(2,3,6)
+plotSivAsymmBands(COMPASS2009,numreplicas_SIDIS,'k+','phT')
+#### Here the user needs to define the plot title based on the data set ####
+plt.savefig('COMPASS09_'+str(LOSSFN)+'_'+str(EPOCHS)+'Ep_'+str(HL)+'H_'+str(NODES)+'N_'+str(LR)+'LR.pdf', format='pdf', bbox_inches='tight')
+
+fig4=plt.figure(4,figsize=(15,10))
+plt.subplot(2,3,1)
+plotSivAsymmBands(COMPASS2015,numreplicas_SIDIS,'pi+','x')
+plt.subplot(2,3,2)
+plotSivAsymmBands(COMPASS2015,numreplicas_SIDIS,'pi+','z')
+plt.subplot(2,3,3)
+plotSivAsymmBands(COMPASS2015,numreplicas_SIDIS,'pi+','phT')
+plt.subplot(2,3,4)
+plotSivAsymmBands(COMPASS2015,numreplicas_SIDIS,'k+','x')
+plt.subplot(2,3,5)
+plotSivAsymmBands(COMPASS2015,numreplicas_SIDIS,'k+','z')
+plt.subplot(2,3,6)
+plotSivAsymmBands(COMPASS2015,numreplicas_SIDIS,'k+','phT')
+#### Here the user needs to define the plot title based on the data set ####
+plt.savefig('COMPASS15_'+str(LOSSFN)+'_'+str(EPOCHS)+'Ep_'+str(HL)+'H_'+str(NODES)+'N_'+str(LR)+'LR.pdf', format='pdf', bbox_inches='tight')
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Sivers_Function_Extraction/NN_Fits/NN_SIDIS/Training_with_RealData_v2/NN_SIDIS_Plots.py
@@ -0,0 +1,241 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import lhapdf
+import matplotlib.pyplot as plt
+import os
+import functions_develop
+from Input_Parameterization import *
+
+
+#Models_Folder = 'Models_SIDIS'
+Models_Folder = '/scratch/cee9hc/Sivers_NN_Models/Train_Real/Models_SIDIS_v4'
+ModelsArray = os.listdir(Models_Folder)
+NNModels = len(ModelsArray)
+
+
+
+class A0(tf.keras.layers.Layer):
+    def __init__(self, kperp2avg=.57, pperp2avg=.12, **kwargs):
+        super(A0, self).__init__(name='a0')
+        self.m1 = tf.Variable(1., name='m1')
+        #self.m1 = tf.Variable(35., name='m1')
+        self.kperp2avg = kperp2avg
+        self.pperp2avg = pperp2avg
+        self.e = tf.constant(1.)
+        
+    def get_config(self):
+        config = super().get_config().copy()
+        
+        config.update({
+            'kperp2avg': self.kperp2avg,
+            'pperp2avg': self.pperp2avg
+        })
+        
+        return config
+        
+    def call(self, inputs):
+        z = inputs[:, 0]
+        pht = inputs[:, 1]
+        ks2avg = (self.kperp2avg*self.m1**2)/(self.m1**2 + self.kperp2avg) 
+        topfirst = (z**2 * self.kperp2avg + self.pperp2avg) * ks2avg**2
+        bottomfirst = (z**2 * ks2avg + self.pperp2avg)**2 * self.kperp2avg 
+        exptop = pht**2 * z**2 * (ks2avg - self.kperp2avg) 
+        expbottom = (z**2 * ks2avg + self.pperp2avg) * (z**2 * self.kperp2avg + self.pperp2avg) 
+        last = tf.sqrt(2*self.e) * z * pht / self.m1      
+        return (topfirst/bottomfirst) * tf.exp(-exptop/expbottom) * last
+
+class Quotient(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super(Quotient, self).__init__()
+    
+    def call(self, inputs):
+        if len(inputs) != 2 or inputs[0].shape[1] != 1:
+            raise Exception('must be two tensors of shape (?, 1)')
+        return inputs[0]/inputs[1]    
+
+def h(model, kperp):
+    m1 = model.get_layer('a0').m1.numpy()
+    e = model.get_layer('a0').e.numpy()
+    print('**********************')
+    print(m1)
+    print(e)
+    print('**********************')
+    return np.sqrt(2*e) * (kperp/m1) * np.exp(-kperp**2/m1**2)
+
+
+def nnq(model, x, hadronstr):
+    if not hadronstr in ['nnu', 'nnd', 'nns', 'nnubar', 'nndbar', 'nnsbar']:
+        raise Exception('hadronstr must be one of nnu, nnd, nns, nnubar, nndbar, nnsbar')
+    mod_out = tf.keras.backend.function(model.get_layer(hadronstr).input,
+                                       model.get_layer(hadronstr).output)
+    return mod_out(x)
+
+
+def fqp(x, QQ, kperp2avg, kperp, flavor):
+    #had = functions_new.Hadron()
+    had = functions_develop.Hadron()
+    fq = had.pdf(flavor, x, QQ)
+    return fq*(1/(np.pi*kperp2avg))*np.exp(-kperp**2/kperp2avg)
+
+def xsivdist(model, x, QQ, kperp2avg, flavor, kperp):
+    refDict = {-3: 'nnsbar',
+               -2: 'nnubar',
+               -1: 'nndbar',
+               1: 'nnd',
+               2: 'nnu',
+               3: 'nns'}
+    nnqval = nnq(model, np.array([x]), refDict[flavor])
+    #nnqval = nnq(model , np.array([x]), refDict[flavor])[:,0]
+    hval = h(model, kperp)
+    fqpval = fqp([x], [QQ], kperp2avg, kperp, flavor)
+    #print(nnqval)
+    #print((2*nnqval)[0, :][0])
+    return ((2*nnqval*hval*fqpval)[0, :])
+    #return ((2*x*hval*fqpval))
+    #return ((2*nnqval)[0, :])
+    #return ((2*x*nnqval))
+
+
+
+def xsivdistFromReplicas(numReplicas, x, QQ, kperp2avg, flavor, kperp):
+    results = []
+    for i in range(numReplicas):
+        t = tf.keras.models.load_model(Models_Folder+'/'+ModelsArray[i], 
+                                         custom_objects={'A0': A0, 'Quotient': Quotient})
+        results.append(list(xsivdist(t, x, QQ, kperp2avg, flavor, kperp)))
+    return np.array(results)
+
+
+#################################################################################
+############################## From Pseudo-data #################################
+#################################################################################
+
+
+# def NN(x,Nq,aq,bq):
+#     tempNNq = Nq*(x**aq)*((1-x)**(bq))*((aq+bq)**(aq+bq))/((aq**aq)*(bq**bq))
+#     return tempNNq
+
+# def NNanti(x,Nqbar):
+#     tempNNqbar = Nqbar
+#     return tempNNqbar
+
+
+def hTh(m1, kperp):
+    ee=1
+    return np.sqrt(2*ee) * (kperp/m1) * np.exp(-kperp**2/m1**2)
+
+def Pseudo_SiversQ(x, n, a, b, m1, QQ, kperp2avg, kperp, flavor):
+    nnqval = NN(x, n, a, b)
+    hval = hTh(m1, kperp)
+    fqpval = fqp([x], [QQ], kperp2avg, kperp, flavor)
+    #plt.plot(kperp,2*nnqval*hval*fqpval)
+    return ((2*nnqval*hval*fqpval))
+    #return ((2*x*hval*fqpval))
+    #return ((x*nnqval))
+
+def Pseudo_SiversAntiQ(x, n, a, b, m1, QQ, kperp2avg, kperp, flavor):
+    nnqval = NNanti(x, n, a, b)
+    hval = hTh(m1, kperp)
+    fqpval = fqp([x], [QQ], kperp2avg, kperp, flavor)
+    #plt.plot(kperp,2*nnqval*hval*fqpval)
+    return ((2*nnqval*hval*fqpval))
+    #return ((2*x*hval*fqpval))
+    #return ((x*nnqval))
+
+
+def Pseudo_Sivers_Q_Plots(x, m1, Nu, Au, Bu, Nd, Ad, Bd, Ns, As, Bs, QQ, kperp2avg, kperp):
+    kp = kperp
+    SivU=Pseudo_SiversQ(x, Nu, Au, Bu, m1, QQ, kperp2avg, kperp, 2)
+    plt.plot(kperp, SivU,'--', color='lightblue', label='$u_{true}$')
+    SivD=Pseudo_SiversQ(x, Nd, Ad, Bd, m1, QQ, kperp2avg, kperp, 1)
+    plt.plot(kperp, SivD,'--', color='tomato', label='$d_{true}$')
+    SivS=Pseudo_SiversQ(x, Ns, As, Bs, m1, QQ, kperp2avg, kperp, 3)
+    plt.plot(kperp, SivS,'--', color='lightgreen', label='$s_{true}$')
+
+def Pseudo_Sivers_AntiQ_Plots(x, Nub, Aub, Bub, Ndb, Adb, Bdb, Nsb, Asb, Bsb, m1, QQ, kperp2avg, kperp):
+    kp = kperp
+    SivU=Pseudo_SiversAntiQ(x, Nub, Aub, Bub, m1, QQ, kperp2avg, kperp, -2)
+    plt.plot(kperp, SivU,'--', color='lightblue', label='$\\bar{u}_{true}$')
+    SivD=Pseudo_SiversAntiQ(x, Ndb, Adb, Bdb, m1, QQ, kperp2avg, kperp, -1)
+    plt.plot(kperp, SivD,'--', color='tomato', label='$\\bar{d}_{true}$')
+    SivS=Pseudo_SiversAntiQ(x, Nsb, Asb, Bsb, m1, QQ, kperp2avg, kperp, -3)
+    plt.plot(kperp, SivS,'--', color='lightgreen', label='$\\bar{s}_{true}$')
+    
+
+    
+    
+def plotSivDistBands(numReplicas, x, QQ, kperp2avg, kperp, numSigma=1):
+    results = xsivdistFromReplicas(numReplicas, x, QQ, kperp2avg, 2, kperp)
+    yhat = results.mean(axis=0)
+    yerr = results.std(axis=0)
+    #print(yerr)
+
+    plt.fill_between(kperp, yhat-numSigma*yerr, yhat+numSigma*yerr,
+                     facecolor='b', alpha=0.3)
+    plt.plot(kperp, yhat, 'b', label='$u$')
+    
+    results = xsivdistFromReplicas(numReplicas, x, QQ, kperp2avg, 1, kperp)
+    yhat = results.mean(axis=0)
+    yerr = results.std(axis=0)
+    
+    plt.fill_between(kperp, yhat-numSigma*yerr, yhat+numSigma*yerr,
+                     facecolor='r', alpha=0.3)
+    plt.plot(kperp, yhat, 'r', label='$d$')
+    
+    results = xsivdistFromReplicas(numReplicas, x, QQ, kperp2avg, 3, kperp)
+    yhat = results.mean(axis=0)
+    yerr = results.std(axis=0)
+    
+    plt.fill_between(kperp, yhat-numSigma*yerr, yhat+numSigma*yerr,
+                     facecolor='g', alpha=0.3)
+    plt.plot(kperp, yhat, 'g', label='$s$')
+    #Pseudo_Sivers_Q_Plots(0.1, 1, 0.2, 0.2, 1, 2.4,  .57,  np.array(list(range(150)))/100)
+    #Pseudo_Sivers_Q_Plots(0.1, m1v,Nuv,auv,buv,Ndv,adv,bdv,Nsv,asv,bsv, 2.4,  .57,  np.array(list(range(150)))/100)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    #plt.ylim(-0.01,0.01)
+    plt.legend(loc=4,fontsize=20,handlelength=3)
+    plt.savefig('SiversQ_SIDIS_NN_RealD.pdf', format='pdf', bbox_inches='tight')
+    
+    
+def plotSivDistBandsSea(numReplicas, x, QQ, kperp2avg, kperp, numSigma=1):
+    #datann = DataANN()
+    #X, y, err = datann.makeData(df, [hadron], [dependence])
+
+    results = xsivdistFromReplicas(numReplicas, x, QQ, kperp2avg, -2, kperp)
+    yhat = results.mean(axis=0)
+    yerr = results.std(axis=0)
+
+    plt.fill_between(kperp, yhat-numSigma*yerr, yhat+numSigma*yerr,
+                     facecolor='b', alpha=0.3)
+    plt.plot(kperp, yhat, 'b', label='$\\bar{u}$')
+    
+    results = xsivdistFromReplicas(numReplicas, x, QQ, kperp2avg, -1, kperp)
+    yhat = results.mean(axis=0)
+    yerr = results.std(axis=0)
+    
+    plt.fill_between(kperp, yhat-numSigma*yerr, yhat+numSigma*yerr,
+                     facecolor='r', alpha=0.3)
+    plt.plot(kperp, yhat, 'r', label='$\\bar{d}$')
+    
+    results = xsivdistFromReplicas(numReplicas, x, QQ, kperp2avg, -3, kperp)
+    yhat = results.mean(axis=0)
+    yerr = results.std(axis=0)
+    
+    plt.fill_between(kperp, yhat-numSigma*yerr, yhat+numSigma*yerr,
+                     facecolor='g', alpha=0.3)
+    plt.plot(kperp, yhat, 'g', label='$\\bar{s}$')
+    #Pseudo_Sivers_AntiQ_Plots(0.1, 0.5, 1, 2.4,  .57,  np.array(list(range(150)))/100)
+    #Pseudo_Sivers_AntiQ_Plots(0.1, Nubv, aubv, bubv, Ndbv, adbv, bdbv, Nsbv, asbv, bsbv, m1v, 2.4,  .57,  np.array(list(range(150)))/100)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    #plt.ylim(-0.001,0.001)
+    plt.legend(loc=1,fontsize=20,handlelength=3)
+    plt.savefig('SiversAntiQ_SIDIS_NN_RealD.pdf', format='pdf', bbox_inches='tight')     
+    
+
+#plt.figure(1)    
+#plotSivDistBands(NNModels, 0.1, 2.4, .57, np.array(list(range(150)))/100)
+plt.figure(2)
+plotSivDistBandsSea(NNModels, 0.1, 2.4, .57, np.array(list(range(150)))/100)
