ebd1e36b7be8390e9857118d43ec29f58c6839b4
isharafernando <isharafernando@gmail.com>
2022-06-30 15:25:22 -0400

    optimized code for fits with real-data

---
+++ Sivers_Function_Extraction/NN_Fits/NN_SIDIS/Training_with_RealData_v3/jobplot.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#SBATCH -p standard
+#SBATCH --output=result_%a.out
+#SBATCH -c 16
+#SBATCH -t 10:00:00
+#SBATCH -A ishara
+
+module load anaconda cuda/11.4.2 cudnn/8.2.4.15
+module load gcc/9.2.0 lhapdf/6.3.0
+source activate lhapdf-tf
+export PYTHONPATH=~/.conda/envs/lhapdf-tf/lib/python3.8/site-packages:$PYTHONPATH
+
+python3 NN_SIDIS_Plots.py
