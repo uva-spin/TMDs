7143567315f2e7eae0a99968f8ff682cee981834
isharafernando <isharafernando@gmail.com>
2025-04-06 08:19:48 -0500

    added files for both models

---
+++ UnpolarizedTMDs_Extraction/Tests_with_E288_Kinematics/Ansatz_Approach_for_QM/Work/Paper/Initial_Exp_Data_Fits/Implementing_both_Sq_and_B/With_Separate_Sq_and_BQM_models/functions_and_constants.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+Num_Replicas =  100
+
+
+eu2 = (2/3)**2
+ed2 = (-1/3)**2
+es2 = (-1/3)**2
+
+
+
+def fDNNQ(QM, b=0.5):
+    return b*QM
+
+# def fDNNQ(QM, b=4):
+#     return 10*np.exp(-((QM-b)**2)/b**2)
+
+# def fDNNQ(QM, b=0.1):
+#     return 10*np.exp(-b*QM)
+    
+# def fDNNQ(QM, b=0.5):
+#     return b*QM
+
+
+alpha = 1/137
+
+hc_factor = 3.89 * 10**8
+
+factor = ((4*np.pi*alpha)**2)/(9*2*np.pi)
+
+
+# Compute S(qT) Contribution
+mm = 0.55
+
+
+## Sk1 ####
+def Sk(k):
+    return ((k**2)/(mm*np.pi))*np.exp(-(k**2)/mm)
+
+
+# ## Sk2 ####
+# def Sk(k):
+#     return ((1)/(mm*np.pi))*np.exp(-(k**2)/mm)
+
+# SqT 1 ##
+def SqT(qT):
+    return (8 * mm * mm + qT**4) / (32 * np.pi * mm) * np.exp(-qT**2 / (2 * mm))
+
+
+# ## SqT 2 ##
+# def SqT(qT):
+#     return 1/ (2 * np.pi * mm) * np.exp(-qT**2 / (2 * mm))
